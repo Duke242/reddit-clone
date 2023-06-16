db330c92640bde1668f287d244808ba40e567edf
Duke 0 <moa@Dukes-MacBook-Air.local>
2023-06-16 01:04:28 -0400

    gradient and info put in the picture container

--- src/PictureInfoBlock.js
+++ src/PictureInfoBlock.js
@@ -5,13 +5,15 @@ function pictureInfoBlock({url}) {
   return (
     <div className='pictureContainer' style={
         {
-          backgroundImage: `url(${url})`,
+          backgroundImage:  `url(${url})`,
           backgroundSize: 'cover',
           backgroundPosition: 'center',
           backgroundRepeat: 'no-repeat',
         }
     }>
         <h2 className='title'>Picture</h2>
+        <p className='titleInfo'>More info about the title</p>
+        <p className='userInfo'>User who posted</p>
     </div>
   )
 }
